1d5c553e29387218b44b8c0f5d8fb357cbd4179e
Danny Avila <danny@librechat.ai>
2025-03-20 16:57:08 -0400

    chore: lint

---
+++ eslint.config.mjs
@@ -0,0 +1,96 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import _import from "eslint-plugin-import";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "dist/**/*",
+    "config/**/*",
+    "routes/**/*",
+    "**/*.js",
+    "**/*.mjs",
+    "src/proto/",
+    "src/scripts/",
+]), {
+    extends: fixupConfigRules(compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+    )),
+
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        import: fixupPluginRules(_import),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2021,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+                project: "./tsconfig.json",
+            },
+        },
+    },
+
+    rules: {
+        "no-trailing-spaces": "error",
+        indent: ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "single"],
+        semi: ["error", "always"],
+
+        "no-multiple-empty-lines": ["error", {
+            max: 1,
+            maxEOF: 0,
+        }],
+
+        "no-console": "warn",
+        "prefer-const": "error",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/consistent-type-assertions": "error",
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-unnecessary-condition": "warn",
+        "@typescript-eslint/strict-boolean-expressions": "warn",
+    },
+}, {
+    files: ["src/stream.ts", "src/utils/logging.ts"],
+
+    rules: {
+        "no-console": "off",
+    },
+}]);
